d6529c665ca618384bf83237f6b246ca63740d83
Jenita John <jjohnpau@ryerson.ca>
2022-04-06 22:22:28 -0400

    Jun's UI changes combined with mine working welll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, jsonify
 from camera import VideoCamera
 import time
 import threading
@@ -8,14 +8,84 @@ import numpy as np
 import cv2
 import sys
 
+
+# connect_database
+import sqlite3
+MYDIR = os.path.dirname(__file__)
+SQLPATH = os.path.join(MYDIR,"db.sqlite3")
+conn = sqlite3.connect(SQLPATH, check_same_thread=False)
+
+# clear_database
+sql = "delete from data"
+cursor = conn.cursor()
+cursor.execute(sql)
+conn.commit()
+cursor.close()
+
+# insert_data
+def insert_data(y):
+    """
+    insert_data
+    """
+    cursor = conn.cursor()
+    sql = f'''
+        insert into data (create_at, y)
+        values (strftime('%Y-%m-%d %H:%M:%f', 'now'), {y})
+    '''
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+
+# get_lastest_data
+def get_latest_data():
+    cursor = conn.cursor()
+    sql = f'''
+        select * from data order by id desc limit 1
+    '''
+    cursor.execute(sql)
+    data = cursor.fetchall()[0]
+    cursor.close()
+    return data
+
 pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 
 # App Globals (do not edit)
 app = Flask(__name__)
 
+# get_latest 
+@app.route('/api/latest')
+def get_latest():
+    sql = f'''
+        select * from data order by id desc limit 1
+    '''
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    try:
+        data = cursor.fetchall()[0]
+        json_data = {
+            'error': 0,
+            'data': {
+                'id': data[0],
+                'create_at': data[1],
+                'y': data[2]
+            }
+        }
+        cursor.close()
+    except:
+        json_data = {
+            'error': 1
+        }
+    return jsonify(json_data)
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html') #you can customze index.html here
+
+#current_live_camera 
 @app.route('/')
 def index():
-    return render_template('index.html') #you can customze index.html here
+    return render_template('index.html')
 
 def buildGauss(frame,levels):
         pyramid = [frame]
@@ -23,7 +93,7 @@ def buildGauss(frame,levels):
             frame = cv2.pyrDown(frame)
             pyramid.append(frame)
         return pyramid
-
+        
 def gen(camera):
     
     realWidth = 320
@@ -97,12 +167,16 @@ def gen(camera):
             
             if i > bpmBufferSize:
                 print(bpmBuffer.mean())
+                # insert_data
+                y = bpmBuffer.mean()
+                insert_data(y)
+                
                 cv2.putText(frame, "BPM: %d" % bpmBuffer.mean(), bpmTextLocation, font, fontScale, fontColor, lineType)
             else:
-               print( "Calculating BPM...")
+               print ("Calculating BPM...")
                cv2.putText(frame, "Calculating BPM...",(0,100), font, 2, (255,255,255), 3)
             
-            #cv2.putText(frame, "Calculating BPM...", loadingTextLocation, font, fontScale, fontColor, lineType)
+            # cv2.putText(frame, "Calculating BPM...", loadingTextLocation, font, fontScale, fontColor, lineType)
             frame = frame.tobytes()
             yield (b'--frame\r\n' 
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
@@ -117,7 +191,7 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='192.168.56.1', debug=False)
+    app.run(host='192.168.56.1', debug=True)
     
 
 
